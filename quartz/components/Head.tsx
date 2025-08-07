14bc519883931d98ed5b5633077233133a4f3bbf
vbr <vadibar@gmail.com>
2025-08-06 18:00:43 -0700

    Customizations: added Markmap support

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -40,6 +40,9 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
+        {/* VBE  Add this inside the <head> section */}
+        <script src="https://cdn.jsdelivr.net/npm/markmap-autoloader@0.15.3"></script>
+        {/* VBE   */} 
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
